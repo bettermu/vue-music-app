ed3ffba7c11aee075fbd93f6062a1104689cf81d
bettermu <1113783908@qq.com>
2018-09-28 17:24:17 +0800

    引入vuex数据管理

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,5 +1,8 @@
 <template>
+<transition name="slide">
   <div class="singer-detail"></div>
+</transition>
+
 </template>
 
 <script>
@@ -19,4 +22,9 @@ export default {
     right:0
     bottom:0
     background:$color-background
+
+  .slide-enter-active,.slide-leave-active
+    transition: all 0.3s
+  .slide-enter,.slide-leave-to
+    transform:translate3d(100%,0,0)
 </style>
