7646364e7fe0b702b2fb954c9079a1fd1f3fbb7c
bettermu <1113783908@qq.com>
2018-10-01 10:12:58 +0800

    歌曲数据格式处理

--- src/api/singer.js
+++ src/api/singer.js
@@ -4,6 +4,7 @@ import {
   options
 } from './config'
 
+//获取歌手列表
 export function getSingerList(){
   const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
   const data = Object.assign({}, commonParams, {
@@ -16,5 +17,23 @@ export function getSingerList(){
     needNewCode: 0,
     platform: 'yqq'
   })
+  return jsonp(url, data, options)
+}
+
+//获取歌手详情
+export function getSingerDetail(singerId){
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    hostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    order: 'listen',
+    begin: 0,
+    num: 80,
+    songstatus: 1,
+    singermid: singerId
+  })
+
   return jsonp(url, data, options)
 }
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -6,31 +6,71 @@
 </template>
 
 <script>
-import {mapGetters} from 'vuex'
+import { mapGetters } from "vuex";
+import { getSingerDetail } from "api/singer";
+import { ERR_OK } from "api/config";
+import { createSong } from "common/js/song";
+
 export default {
-  computed:{
-    ...mapGetters(['singer'])
+  data() {
+    return {
+      songs: []
+    };
   },
-  created(){
-    console.log(this.singer)
+  computed: {
+    ...mapGetters(["singer"])
+  },
+  created() {
+    this._getDetail(this.singer.id);
+    //console.log(this.singer)
+  },
+  methods: {
+    _getDetail(singerId) {
+      if (!singerId) {
+        this.$router.push("/singer");
+        return;
+      }
+      getSingerDetail(singerId).then(res => {
+        if (res.code === ERR_OK) {
+          //console.log(res.data.list);
+          this.songs=this._normalizeSongs(res.data.list)
+          console.log(this.songs)
+        }
+      });
+    },
+    _normalizeSongs(list) {
+      let ret = [];
+      list.forEach(item => {
+        let { musicData } = item;
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData));
+        }
+      });
+
+      return ret
+    }
   }
-}
+};
 </script>
 
 <style lang="stylus" scoped>
-  @import "~common/stylus/variable"
+@import '~common/stylus/variable';
 
-  .singer-detail
-    position:fixed
-    z-index:100
-    top:0
-    left:0
-    right:0
-    bottom:0
-    background:$color-background
+.singer-detail {
+  position: fixed;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: $color-background;
+}
 
-  .slide-enter-active,.slide-leave-active
-    transition: all 0.3s
-  .slide-enter,.slide-leave-to
-    transform:translate3d(100%,0,0)
+.slide-enter-active, .slide-leave-active {
+  transition: all 0.3s;
+}
+
+.slide-enter, .slide-leave-to {
+  transform: translate3d(100%, 0, 0);
+}
 </style>
