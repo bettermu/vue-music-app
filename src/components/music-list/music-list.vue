4ffe93428da2497d95cfb19d8d6c6dd1247b09dc
bettermu <1113783908@qq.com>
2018-10-03 14:27:19 +0800

    歌手歌曲列表页面loading效果添加

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div class="play-wrapper">
+        <div ref="playBtn" class="play" v-show="songs.length">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
     <scroll @scroll="scroll" :data="songs" :probe-type="probeType" :listen-scroll="listenScroll"  class="list" ref="list">
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
+      </div>
+      <div class="loading-container" v-show="!songs.length">
+        <loading></loading>
       </div>
     </scroll>
   </div>
@@ -19,6 +28,7 @@
 <script>
 import Scroll from "base/scroll/scroll";
 import SongList from "base/song-list/song-list";
+import Loading from 'base/loading/loading'
 import {prefixStyle} from 'common/js/dom'
 
 const transform=prefixStyle('transform')
@@ -62,6 +72,9 @@ export default {
   methods: {
     scroll(pos) {
       this.scrollY = pos.y;
+    },
+    back(){
+      this.$router.back()
     }
   },
   watch: {
@@ -86,9 +99,11 @@ export default {
         zIndex = 10;
         this.$refs.bgImage.style.paddingTop = 0;
         this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`;
+        this.$refs.playBtn.style.display='none'
       } else {
         this.$refs.bgImage.style.paddingTop = "70%";
         this.$refs.bgImage.style.height = 0;
+        this.$refs.playBtn.style.display=''
       }
       this.$refs.bgImage.style["transform"] = `scale(${scale})`;
       this.$refs.bgImage.style.zIndex = zIndex;
@@ -96,7 +111,8 @@ export default {
   },
   components: {
     Scroll,
-    SongList
+    SongList,
+    Loading
   }
 };
 </script>
