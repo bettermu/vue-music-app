162495f4d169448266514e9e7ca245e85da07c16
bettermu <1113783908@qq.com>
2018-10-03 15:50:13 +0800

    播放器vuex数据编写

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,19 @@
+import {playMode} from 'common/js/config'
+
 const state={
-  singer:{}
+  singer:{},
+  //是否播放
+  playing:false,
+  //全屏状态
+  fullScreen:false,
+  //播放列表
+  playlist:[],
+  //
+  sequenceList:[],
+  //播放模式
+  mode:playMode.sequence,
+  //当前播放索引
+  currentIndex:-1
 }
 
 export default state
